59fac18c9a97b80090990fb85daa9132bca7b9b2
Nick Franken <shnick@gmail.com>
2016-03-25 00:39:02 -0500

    need those

--- spec/places_spec.cr
+++ spec/places_spec.cr
@@ -3,13 +3,13 @@ require "./spec_helper"
 describe GoogleMapsApi do
 	describe "Place" do
 		describe "#nearby" do
-			# it "should throw an error without a radius" do
-			# 	expect_raises(Exception) {GoogleMapsApi::Place.nearby(40.714224, -73.961452)}
-			# end
+			it "should throw an error without a radius" do
+				expect_raises(Exception) {GoogleMapsApi::Place.nearby(40.714224, -73.961452)}
+			end
 
-			# it "should throw an error without an api key" do
-			# 	expect_raises(GoogleMapsApi::Errors::ClientError) { GoogleMapsApi::Place.nearby(40.714224, -73.961452, {radius: 1000}) }
-			# end
+			it "should throw an error without an api key" do
+				expect_raises(GoogleMapsApi::Errors::ClientError) { GoogleMapsApi::Place.nearby(40.714224, -73.961452, {radius: 1000}) }
+			end
 
 			it "should return an array of StreetAddresses" do
 				# how to set env variable???
